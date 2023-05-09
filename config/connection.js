90c6dfcf5664486e295da2ec049ab20d5ef63daf
eromeroguti <eromeroguti@gmail.com>
2023-05-08 18:53:36 -0700

    ecommerce homework

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const sequelize = require('sequelize');
+require('dotenv').config();
+
+
+const Sequelize = new sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
+module.exports = Sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS ecommerceBuidl_db;
+CREATE DATABASE ecommerceBuidl_db;
+
